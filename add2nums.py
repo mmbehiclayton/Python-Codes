f03af1fdd76c8174d044df3b7a904e645b0b85aa
Clayton Mmbehi <30318074+mmbehiclayton@users.noreply.github.com>
2023-04-10 23:19:28 +0300

    add two numbers in Python

---
+++ add2nums.py
@@ -0,0 +1,5 @@
+x=int(input("Enter the value of X: "))
+y=int(input("Enter the value of y: "))
+
+sum = x+y
+print(f"The sum of {x} and {y} is: {sum}")
---
+++ python-output.py
@@ -0,0 +1,13 @@
+print(80)    # Outputs: 80
+ 
+# print a string
+print('Clayton Mmbehi')    # Output: Clayton Mmbehi
+ 
+# create a variable
+city = 'London'
+ 
+# print the city variable
+print(city)     # Output: London
+ 
+# print multiple items at once
+print(22, 'Batman')    # Output: 22 Batman
