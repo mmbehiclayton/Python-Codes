014de0f8fa7c39c08cb93068e89e7ef2a462cc50
Clayton Mmbehi <30318074+mmbehiclayton@users.noreply.github.com>
2023-04-10 22:40:31 +0300

    user input in python
    python program with user input

---
+++ use-input.py
@@ -0,0 +1,6 @@
+#python program that prompts user input
+name = input("Enter your FullName: ")
+Age = input("Enter Your Age:")
+
+#display the result on the screen, use print function
+print(f"Hello {name} you are {Age} Years Old")
